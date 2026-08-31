465895465c0b64229944ce10b98f3b25aa537536
jashwanth466 <jaswanthnaidu466@gmail.com>
2026-08-31 15:58:18 +0530

    Add files via upload
    I am jashwanth naidu

---
+++ comparision_operation.py
@@ -0,0 +1,10 @@
+#python comparision operators Exercise
+a=50
+b=20
+result=a>b # Greater than comparision operator
+print("result of",a,">",b,"is:",result)
+
+
+
+result=a<b # Less than comparision operator
+print("result of",a,"<",b,"is:",result)
